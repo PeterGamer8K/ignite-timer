dbbff7848c81ef6954a01af539823eb7cec57a54
Pedro Meira <petergamer8k@gmail.com>
2022-09-27 20:23:44 -0300

    chore: adding styled components lib & creating global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
-function App() {
-  return <h1>Hello world!</h1>
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World!!!</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
-
-export default App
